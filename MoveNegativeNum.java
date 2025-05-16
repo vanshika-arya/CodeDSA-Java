d9f335e2acb882343cabaf223db0774e9aed15d1
vanshika-arya <vanshika3915@gmail.com>
2025-05-17 00:17:36 +0530

    move all the negative number before positive numbers

---
+++ MoveNegativeNum.java
@@ -0,0 +1,18 @@
+public class MoveNegativeNum {
+    public static void main(String[] args) {
+        int arr[]={1,-8,5,-29,-56,9};
+        int j=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<0){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+                j++;
+            }
+        }
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    
+}
