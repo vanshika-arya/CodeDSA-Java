f2ad0ebf7d94904103c7f3162b6d064be8e3e9fb
vanshika-arya <vanshika3915@gmail.com>
2025-05-24 22:53:29 +0530

    Maximum Product subarray

---
+++ MaxiMumProductSubarray.java
@@ -0,0 +1,17 @@
+public class MaxiMumProductSubarray {
+    public static void main(String[] args) {
+        int arr[]={10,-2,34,-78,0,14,11};
+        int maxproduct=arr[0];
+        for(int i=0;i<arr.length;i++){
+            int product=1;
+            for(int j=i;j<arr.length;j++){
+                product*=arr[j];
+                if(product>maxproduct)
+                 maxproduct=product;
+            }
+        }
+        System.out.println("Maximum Product subarray: "+maxproduct);
+
+    }
+    
+}
