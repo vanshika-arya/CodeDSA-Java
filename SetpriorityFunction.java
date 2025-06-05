3424b395bedd610a58a840dbcb04f55c00764a78
vanshika-arya <vanshika3915@gmail.com>
2025-06-05 12:57:42 +0530

    SetPriority and yield method

---
+++ SetpriorityFunction.java
@@ -0,0 +1,23 @@
+class Mythread extends Thread{
+    public void run(){
+        for(int i=1;i<=4;i++){
+            System.out.println(getName()+ " priority "+ getPriority());
+            
+        }
+    }
+}
+public class SetpriorityFunction {
+    public static void main(String[] args) {
+        Mythread t1= new Mythread();
+        Mythread t2= new Mythread();
+        t1.setName("Vanshika");
+        t2.setName("Alshifa");
+
+        t1.setPriority(Thread.MAX_PRIORITY);
+        t2.setPriority(Thread.MIN_PRIORITY);
+
+        t1.start();
+        t2.start();
+    }
+    
+}
--- thread1.java
+++ thread1.java
@@ -1,7 +1,11 @@
 public class thread1 {
     public static void main(String[] args) {
         Thread t1= new Thread( ()-> { System.out.println("hello");
+        try{
+        Thread.sleep(1000);
+        }catch(Exception e){}
     });
+    
      Thread t2= new Thread( ()-> { System.out.println("byee");});
      t1.start();
      t2.start();
