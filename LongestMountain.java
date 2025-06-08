16318f5f8dc53e46e52075791e728c6e75be2cfd
vanshika-arya <vanshika3915@gmail.com>
2025-06-08 22:18:14 +0530

    first element to occur k times

--- LongestMountain.java
+++ LongestMountain.java
@@ -4,17 +4,12 @@ public class LongestMountain {
         int maxLen = 0;
 
         for (int i = 1; i < n - 1; i++) {
-            // Check if current is peak
             if (arr[i - 1] < arr[i] && arr[i] > arr[i + 1]) {
                 int left = i - 1;
                 int right = i + 1;
-
-                // Go left (strictly increasing)
                 while (left > 0 && arr[left - 1] < arr[left]) {
                     left--;
                 }
-
-                // Go right (strictly decreasing)
                 while (right < n - 1 && arr[right] > arr[right + 1]) {
                     right++;
                 }
@@ -24,15 +19,14 @@ public class LongestMountain {
                     maxLen = len;
                 }
 
-                i = right; // optimization to skip already counted part
+                i = right; 
             }
         }
 
         return maxLen;
     }
-
     public static void main(String[] args) {
         int[] arr = {2, 1, 4, 7, 3, 2, 5};
-        System.out.println("Longest mountain length: " + longestMountain(arr)); // Output: 5
+        System.out.println("Longest mountain length: " + longestMountain(arr)); 
     }
 }
