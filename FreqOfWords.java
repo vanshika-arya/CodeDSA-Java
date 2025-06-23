d4aea5a3e0b2500c57b1958e47b807b8e78ac690
vanshika-arya <vanshika3915@gmail.com>
2025-06-23 21:51:05 +0530

    count frequency of each word using collection framework

---
+++ FreqOfWords.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.stream.*;
+
+public class FreqOfWords {
+     public static void main(String[] args) {
+        List<String> words = Arrays.asList("apple", "banana", "apple", "orange", "banana", "apple");
+
+        Map<String, Long> frequency = words.stream()
+                                           .collect(Collectors.groupingBy(word -> word, Collectors.counting()));
+
+        frequency.forEach((word, count) -> System.out.println(word + " -> " + count));
+     }
+}
+
