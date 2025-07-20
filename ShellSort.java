cc3719d7549f2fa5fafa755bf2704545757d4355
vanshika-arya <vanshika3915@gmail.com>
2025-07-20 21:23:58 +0530

    sort array using shell sort

---
+++ ShellSort.java
@@ -0,0 +1,43 @@
+public class ShellSort {
+     static void shellSort(int arr[]) {
+        int n = arr.length;
+
+        // Start with a big gap, then reduce the gap
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            // Do a gapped insertion sort for this gap size.
+            for (int i = gap; i < n; i++) {
+                int temp = arr[i];
+
+                // Shift earlier gap-sorted elements up until the correct location for arr[i] is found
+                int j;
+                for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+                    arr[j] = arr[j - gap];
+                }
+
+                // Put temp (the original arr[i]) in its correct location
+                arr[j] = temp;
+            }
+        }
+    }
+
+    // Function to print the array
+    static void printArray(int arr[]) {
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        int arr[] = {12, 34, 54, 2, 3};
+
+        System.out.println("Original Array:");
+        printArray(arr);
+
+        shellSort(arr);
+
+        System.out.println("Sorted Array:");
+        printArray(arr);
+    }
+}
